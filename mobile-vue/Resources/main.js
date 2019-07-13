85449a6a9eb70449f62b86f1bd7d95cdd04f15d3
Ben Winding <ben.winding@gmail.com>
2019-07-14 00:32:18 +0930

    Even better

--- mobile-vue/Resources/main.js
+++ mobile-vue/Resources/main.js
@@ -1,5 +1,50 @@
 Vue.use(VueMaterial.default);
 
+Vue.component('screen', {
+  template: `
+  <md-card class="screen-card">
+    <span class="screen-url">
+    https://www.example.com/
+    </span>
+    <md-icon class="screen-x">clear</md-icon>
+    <img class="screen-img" src="https://benwinding.github.io/mock-fmlink-menu/pages/background.png" />
+  </md-card>  
+  `
+});
+
+Vue.component('infopanel', {
+  template: `
+<md-card md-with-hover>
+   <md-card-header>
+      <md-card-header-text>
+         <div class="md-title">Media card</div>
+      </md-card-header-text>
+   </md-card-header>
+   <md-card-content>
+    The <span style="font-weight: bold;">Users&nbsp;</span>page&nbsp;
+    <div>
+        <ul>
+          <li>Contractors</li>
+          <li>Staff</li>
+          <li>Add/Remove</li>
+          <li>Correspondence Log (maybe)</li>
+        </ul>
+    </div>
+   </md-card-content>
+   <md-card-actions>
+      <md-button class="md-icon-button md-raised md-primary">
+        <md-icon>chevron_left</md-icon>
+        <md-tooltip md-direction="top">Previous Page</md-tooltip>
+      </md-button>
+      <pre>[1/2]</pre>
+      <md-button class="md-icon-button md-raised md-primary">
+        <md-icon>chevron_right</md-icon>
+        <md-tooltip md-direction="top">Next Page</md-tooltip>
+      </md-button>
+   </md-card-actions>
+</md-card>  `
+});
+
 const App = {
   name: 'App',
   template: `
@@ -20,8 +65,27 @@ const App = {
           </div>
         </div>
       </md-toolbar>
+      <div class="app-screen md-layout md-alignment-center">
+        <div class="md-layout-item md-xlarge-size-70 md-large-size-60 md-xsmall-size-100">
+          <div class="screen-container">
+            <screen />
+          </div>
+        </div>
+        <div class="md-layout-item md-xlarge-size-30 md-large-size-40 md-xsmall-size-100">
+          <div class="info-container">
+            <infopanel />
+          </div>
+        </div>
+      </div>
     </div>
+  `,
+  styles: [
+    `
+    .box {
+      height: 100px;
+    }
   `
+  ]
 };
 
 new Vue({
