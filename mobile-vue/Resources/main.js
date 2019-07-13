4ae4b15ed740c2c3f13b444fbe4231a2a88f60b3
Ben Winding <ben.winding@gmail.com>
2019-07-13 20:14:27 +0930

    Toolbar added

--- mobile-vue/Resources/main.js
+++ mobile-vue/Resources/main.js
@@ -1,6 +1,29 @@
-import App from './components/App.js';
+Vue.use(VueMaterial.default);
 
-// Vue.use(VueMaterial);
+const App = {
+  name: 'App',
+  template: `
+    <div>
+      <md-toolbar class="md-primary">
+        <div class="md-toolbar-row">
+          <div class="md-toolbar-section-start">
+            <md-button class="md-icon-button">
+              <md-icon>view_carousel</md-icon>
+            </md-button>
+            <h3 class="md-title">TITLE</h3>
+          </div>
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button">
+              <md-icon>fullscreen</md-icon>
+            </md-button>
+          </div>
+        </div>
+      </md-toolbar>
+    </div>
+  `
+};
+
 new Vue({
   render: h => h(App)
 }).$mount(`#app`);
